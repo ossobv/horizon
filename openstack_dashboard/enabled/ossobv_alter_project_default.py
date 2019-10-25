2b7f53d9f283554c5b680a9d096cb3700185c744
Walter Doekes <walter+github@wjd.nu>
2019-10-25 18:48:04 +0200

    panels/enable: Remove unused panels/urls
    (And get rid of a warning while we're at it.)

---
+++ openstack_dashboard/enabled/ossobv_alter_project_default.py
@@ -0,0 +1,8 @@
+# Panel settings: /project/<default>/api_access
+PANEL_DASHBOARD = 'project'
+PANEL_GROUP = 'default'
+PANEL = 'api_access'
+
+# The default _was_ "overview", but that gives 403s.
+# Make this the default in this dashboard.
+DEFAULT_PANEL = 'api_access'
